22542130b4734f8bc04280d024ead0cfadf88dc5
Sev J <sev314@gmail.com>
2026-08-28 00:00:37 +0900

    🚀 `docs` 폴더로 배포

---
+++ docs/assets/member-create-BdXsUlCH.js
@@ -0,0 +1,1 @@
+import{i as e}from"./app-header-CZMorswj.js";/* empty css                     */import{t}from"./utils-dom-AA-aQdFf.js";/* empty css                       */import{t as n}from"./member-store-1q3pjhCj.js";import"./member-form-Coqte7C2.js";e();var r=t(`member-form`);r.onSubmit=async e=>{try{await n(e),window.location.href=`members.html`}catch(e){console.error(`회원 등록 실패:`,e)}};
