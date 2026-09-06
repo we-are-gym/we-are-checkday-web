f70fd7fc6c471e20b1330470a60745f683b0e725
Sev J <sev314@gmail.com>
2026-09-06 19:25:16 +0900

    🚀 `docs` 폴더로 배포

--- docs/assets/member-create-GdlNIhmQ.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./app-header-CTxcyEpO.js";/* empty css                     */import{t}from"./utils-dom-AA-aQdFf.js";/* empty css                       */import{t as n}from"./member-store-B747tHKL.js";import"./member-form-DYHMvO60.js";e();var r=t(`member-form`);r.onSubmit=async e=>{try{await n(e),window.location.href=`members.html`}catch(e){console.error(`회원 등록 실패:`,e)}};
