53eab60e55ccfd8f53ca4a6c5ed36cf5532215ca
Sev J <sev314@gmail.com>
2026-09-07 21:09:52 +0900

    🚀 `docs` 폴더로 배포

--- docs/assets/member-form-2D9W_POG.js
+++ docs/assets/member-form-2D9W_POG.js
@@ -1,4 +1,4 @@
-import{t as e}from"./app-header-CBLU3CuN.js";e({tag:`member-form`,render(){return`
+import{t as e}from"./app-header-QCOnakgs.js";e({tag:`member-form`,render(){return`
 			<form class="member-form" id="member-form" novalidate aria-label="회원 정보 입력 폼">
 				<label class="member-field">
 					<span>이름</span>
